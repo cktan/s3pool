db161437032a31ef67cb51b32982c233d32135fa
CK Tan <cktan@vitessedata.com>
2019-10-18 22:18:14 -0700

    updated

--- src/s3pool/main.go
+++ src/s3pool/main.go
@@ -1,16 +1,15 @@
 /*
- * S3pool - S3 cache on local disk
- * Copyright (c) 2019 CK Tan
- * cktanx@gmail.com
+ *  S3pool - S3 cache on local disk
+ *  Copyright (c) 2019 CK Tan
+ *  cktanx@gmail.com
  *
- *
- * S3Pool can be used for free under the GNU General Public License
- * version 3 (where anything released into public must be open source) or
- * under a commercial license if such has been acquired (send email to
- * cktanx@gmail.com). The commercial license does not cover derived or
- * ported versions created by third parties under GPL.
+ *  S3Pool can be used for free under the GNU General Public License
+ *  version 3, where anything released into public must be open source,
+ *  or under a commercial license. The commercial license does not
+ *  cover derived or ported versions created by third parties under
+ *  GPL. To inquire about commercial license, please send email to
+ *  cktan@gmail.com.
  */
-
 package main
 
 import (
