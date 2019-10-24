7e02c1756df6a3107f7d6e9d35f547d7f075a8c8
CK Tan <cktan@vitessedata.com>
2019-10-24 11:04:15 -0700

    pidfile

---
+++ src/s3pool/pidfile/pidfile.go
@@ -0,0 +1,54 @@
+package pidfile
+
+import (
+	"io/ioutil"
+	"strings"
+	"os"
+	"os/exec"
+	"strconv"
+	"fmt"
+
+)
+
+var pidFname string = "pidfile.pid"
+
+func SetFname(s string) {
+	pidFname = s
+}
+
+func Read() int {
+	dat, err := ioutil.ReadFile(pidFname)
+	if err != nil {
+		return 0
+	}
+	pidstr := strings.Trim(string(dat), " \t\r\n")
+	ret, err := strconv.Atoi(pidstr)
+	if err != nil {
+		return 0
+	}
+	return ret
+}
+
+
+func Write() {
+	pid := os.Getpid()
+	byt := []byte(fmt.Sprint(pid))
+	ioutil.WriteFile(pidFname, byt, 0644)
+}
+
+
+
+func PsOutput() string {
+	pid := Read()
+	if pid == 0 {
+		return ""
+	}
+
+	cmd := exec.Command("ps", "-h", "-p", strconv.Itoa(pid))
+	out, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
+
+	return string(out)
+}
