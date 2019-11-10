b558157472ab5115d75599737df63adef229f8b5
CK Tan <cktan@vitessedata.com>
2019-11-09 23:51:39 -0800

    chkpt

--- src/s3pool/mon/mastermon.go
+++ src/s3pool/mon/mastermon.go
@@ -18,33 +18,63 @@ import (
 	"fmt"
 	"bytes"
 	"io"
-	"sync/atomic"
 	"strings"
+	"sync"
 )
 
 
-var master, standby string
-var masterIsDown int32
+type mastermonCb struct {
+	sync.Mutex
+	master, standby string
+	currMaster string
+}
+
+var cb = &mastermonCb{}
 var port int
 
-func Get() string {
-}
-
-
-func SetMasterIsDown(flag bool) {
-	var val int32
-	if flag {
-		val = 1
+func Get(exclude string) string {
+	cb.Lock()
+	defer cb.Unlock()
+	
+	if exclude == cb.currMaster {
+		cb.currMaster = cb.standby
+		if exclude == cb.currMaster {
+			cb.currMaster = cb.master
+		}
 	}
-	atomic.StoreInt32(&masterIsDown, val)
+	return cb.currMaster
+}
+
+func Mastermon(master, standby string, port_ int) error {
+	if master == "" {
+		return fmt.Errorf("mastermon: missing master param")
+	}
+	if standby == "" {
+		standby = master
+	}
+	cb.Lock()
+	cb.master = master
+	cb.standby = standby
+	cb.currMaster = master
+	cb.Unlock()
+
+	port = port_
+	for {
+		time.Sleep(60 * time.Second)
+	}
 }
 
 
-func IsMasterDown() bool {
-	val := atomic.LoadInt32(&masterIsDown)
-	return val != 0
+
+func NetPing(host string) bool {
+	reply, _ := remoteCall(host, []string{"PING"})
+	return len(reply) > 3 && reply[:3] == "OK\n" 
 }
 
+func NetGlob(host, bucket, pattern string) (reply string, err error) {
+	reply, err = remoteCall(host, []string{"GLOBX", bucket, pattern})
+	return
+}
 
 func remoteCall(host string, args []string) (reply string, err error) {
 	var sb strings.Builder
@@ -78,22 +108,3 @@ func remoteCall(host string, args []string) (reply string, err error) {
 	return 
 }
 
-
-
-func netPing(host string) bool {
-	reply, _ := remoteCall(host, []string{"PING"})
-	return len(reply) > 3 && reply[:3] == "OK\n" 
-}
-
-func netGlob(host, bucket, pattern string) (reply string, err error) {
-	reply, err = remoteCall(host, []string{"GLOBX", bucket, pattern})
-	return
-}
-
-func Mastermon(master_, standby_ string, port_ int) {
-	port = port_
-	master, standby = master_, standby_
-	for {
-		time.Sleep(60 * time.Second)
-	}
-}
